0da21bc4f88cb96c2637ea67fc81d99b9f39a04e
gabumwaingo <gabumwaingo@gmail.com>
2023-10-22 19:30:49 -0700

    Task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Calculates the start and end indexes """
 
+from typing import Tuple
 
-def index_range(page, page_size):
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
